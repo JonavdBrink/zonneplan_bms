f6e538813b516cd0b18fd7cd41c93a079768a7b4
Jonathan van den Brink <jonathan@padgin.com>
2025-10-23 20:16:43 +0200

    Fixed price sensor

--- custom_components/zonneplan_peakdetect/const.py
+++ custom_components/zonneplan_peakdetect/const.py
@@ -7,4 +7,4 @@ PEAK_SENSOR = "zonneplan_peak_hour"
 VALLEY_SENSOR = "zonneplan_valley_hour"
 HIGHEST_SENSOR = "zonneplan_is_highest_hour"
 LOWEST_SENSOR = "zonneplan_is_lowest_hour"
-PRICE_SENSOR = "sensor.zonneplan_energy_prices"  # Update this to match your entity
+PRICE_SENSOR = "sensor.zonneplan_current_electricity_tariff"
